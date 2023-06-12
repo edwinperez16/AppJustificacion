bcd35cfedf6ac7c4d04c05ea8feb13531ea4669f
IsaacCM_3010 <93792773+isaac3010hub@users.noreply.github.com>
2023-06-11 20:13:33 -0500

    Primeras partes del Proyecto
    omaga

---
+++ app_Justificaicon/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:app_justificacion/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'JUSTIFICACION',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData.light().copyWith(
+        appBarTheme: AppBarTheme(
+        color: Color(0xFFAD0837),),
+        
+       
+      ),
+
+    );
+  }
+}
