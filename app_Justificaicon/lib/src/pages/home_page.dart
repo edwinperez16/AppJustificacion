3035b41a9e52d6c21e0c6ed0059468ddcbff571c
AlanSuaste32 <93949411+AlanSuaste32@users.noreply.github.com>
2023-06-12 00:05:18 -0500

    Creacion del Formulario
    Creacion del Formulario para insertar las justificaciones

---
+++ app_Justificaicon/lib/src/pages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'crear_justificacion.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('JUSTIFICACION'),
+      ),
+      body: ListView(
+        children: <Widget>[
+         ListTile(
+           title: Text('JUSTIFICACION'),
+           subtitle: Text('Crear una justificacion'),
+           leading: Icon(Icons.insert_drive_file_sharp),
+           trailing: Icon(Icons.keyboard_arrow_right),
+           onTap: () {Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FormularioPage()),
+    );
+             
+           },
+         ),
+          Divider(),
+
+          ListTile(
+            title: Text('BUSCAR'),
+            subtitle: Text('Buscar justificacion'),
+            leading: Icon(Icons.location_searching_rounded),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            
+          ),
+
+        ],
+      ),
+    );
+  }
+}
